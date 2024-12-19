a8da08bb265b9fd93b4e160cfa93c11dd5b3b9ef
GalenReed <galenreed@vt.edu>
2024-12-19 16:19:54 -0500

    songs

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,37 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artitsts(artist)
+        Song.add_to_genre_count(self.genre)
+        Song.add_to_artitst_count(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artitsts(cls, artist):
+        cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+
+    @classmethod
+    def add_to_artitst_count(cls, artist):
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
